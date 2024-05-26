dcf61ae15ff301da0e4670c72b0d22042f4dc87b
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-05-26 11:44:55 +0200

    Test fetching messages

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -1,7 +1,59 @@
 import { ChannelType, Events, Message } from 'discord.js';
 import { DiscordEvent } from '../types/DiscordEvent';
-import buildLongContentEmbeds from '../commands/utilities/buildLongContentEmbeds';
 import cache from '../cache';
+import buildLongContentEmbeds from '../commands/utilities/buildLongContentEmbeds';
+
+const outputMessage = (message: Message, indent?: number) => {
+  const username = message.author.username;
+  let output = indent && indent > 0 ? '  '.repeat(indent) : '';
+
+  // message content exists
+  if (message.content !== '') {
+    output += `[[message:${username}]]: ${message.content}`;
+  }
+  // check if sticker
+  else if (message.stickers.size) {
+    output += `[[sticker:${username}]]`;
+  }
+  // check if attachment
+  else if (message.attachments.size) {
+    output += `[[attachment:${username}]]`;
+  }
+  // check if embed
+  else if (message.embeds.length) {
+    output += `[[embed:${username}]]`;
+  }
+  // check if embed
+  else {
+    output += `[[generic:${username}]]`;
+  }
+
+  console.log(output);
+};
+
+const handleReplyChainHelper = async (
+  message: Message,
+  depth: number,
+  count: number
+) => {
+  if (count > depth) return;
+  if (!message.reference?.messageId) return;
+
+  const replyMessage = await message.channel.messages.fetch(
+    message.reference.messageId
+  );
+
+  // check reference id and the fetched message, this tells us if
+  // the message isn't deleted (by the user)
+  if (replyMessage && message.reference.messageId === replyMessage.id) {
+    outputMessage(replyMessage, count);
+    await handleReplyChainHelper(replyMessage, depth, count + 1);
+  }
+};
+
+const handleReplyChain = async (message: Message, depth: number) => {
+  await handleReplyChainHelper(message, depth, 1);
+};
 
 export default {
   name: Events.MessageCreate,
@@ -14,6 +66,15 @@ export default {
     if (channel.type !== ChannelType.GuildText) return;
 
     if (message.content === '') return;
+
+    const messages = await message.channel.messages.fetch({
+      limit: 10,
+      before: '1244219469983780946',
+    });
+    for (const [_id, message] of messages) {
+      outputMessage(message);
+      await handleReplyChain(message, 3);
+    }
 
     const webhook = await cache.webhook.get(channel);
     webhook.send({
