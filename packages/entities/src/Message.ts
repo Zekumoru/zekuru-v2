fd2754dce5973b43b4e0f003d32898e62cb59947
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-12-09 11:44:20 +0100

    Create Message entity (I forgot)

---
+++ packages/entities/src/Message.ts
@@ -0,0 +1,14 @@
+import { CommonEntity, Snowflake } from '@zekuru-v2/types';
+
+export class Message implements CommonEntity {
+  constructor(
+    public _id: Snowflake,
+    public authorId: Snowflake,
+    public channelId: Snowflake,
+    public guildId: Snowflake,
+    public createdAt: Date,
+    public createdBy: Snowflake,
+    public modifiedAt: Date,
+    public modifiedBy: Snowflake
+  ) {}
+}
