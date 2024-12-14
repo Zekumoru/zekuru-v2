84cbc604965c8e929ce4c7bdb7d4abaf674cbcda
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-12-14 13:38:52 +0100

    Fix db missing exports

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,4 +2,7 @@ export * from './mongodb/models//ChannelLink';
 export * from './mongodb/models/GuildKey';
 export * from './mongodb/models/MessageLink';
 export * from './mongodb/models/TranslateChannel';
+export * from './mongodb/Channel';
+export * from './mongodb/Guild';
+export * from './mongodb/Message';
 export * from './mongodb/connect';
