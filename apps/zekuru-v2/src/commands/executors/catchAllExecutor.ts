08d5e7418455573e59f8ff4c787f2a2fb060e61d
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-12-24 14:41:09 +0100

    Create catchAllExecutor

---
+++ apps/zekuru-v2/src/commands/executors/catchAllExecutor.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import { InteractionErrorExecutor } from '@zekuru-v2/types';
+import { ChatInputCommandInteraction } from 'discord.js';
+import { logger } from '@zekuru-v2/utils';
+
+const catchAllExecutor: InteractionErrorExecutor<
+  Error,
+  {},
+  ChatInputCommandInteraction
+> = async (error, { interaction }) => {
+  if (interaction.deferred) {
+    await interaction.editReply(`Generic error occurred: ${error.message}`);
+  } else {
+    await interaction.reply(`Generic error occurred: ${error.message}`);
+  }
+
+  logger.error(error);
+};
+
+export default catchAllExecutor;
