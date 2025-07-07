20db450c2ffd9a05004722cfd5fd90db465b3313
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2025-07-08 01:01:42 +0200

    refactor: Make link manager fetch channels

--- packages/core/src/link/LinkManager.test.ts
+++ packages/core/src/link/LinkManager.test.ts
@@ -1,6 +1,11 @@
 import { Channel, ChannelLanguage } from '@zekuru-v2/entities';
 import { LinkManager } from './LinkManager';
 import { LinkOptions } from './LinkOptions';
+import {
+  CacheManagerRepository,
+  ChannelCacheRepository,
+  ChannelInMemoryRepository,
+} from '@zekuru-v2/repositories';
 
 function createChannel(id: string, links: string[] = []): Channel {
   const now = new Date();
@@ -8,54 +13,72 @@ function createChannel(id: string, links: string[] = []): Channel {
   return new Channel(id, 'guild', links, dummyLang, now, 'user', now, 'user');
 }
 
+function createChannelCacheRepo(): ChannelCacheRepository {
+  return new ChannelCacheRepository(
+    new CacheManagerRepository({}),
+    new ChannelInMemoryRepository(),
+  );
+}
+
 describe('LinkManager', () => {
-  test('/link single source:A target:B mode:non-recursive mono:true', () => {
+  test('/link single source:A target:B mode:non-recursive mono:true', async () => {
+    const repo = createChannelCacheRepo();
     const a = createChannel('A');
+    await repo.set(a._id, a);
     const b = createChannel('B');
-    const manager = new LinkManager([a, b]);
+    await repo.set(b._id, b);
+    const manager = new LinkManager(repo);
     const options: LinkOptions = {
       single: true,
       mono: true,
       mode: 'non-recursive',
     };
 
-    manager.link([a, b], options);
+    await manager.link([a, b], options);
 
     // Expect A -> B but not vice-versa
     expect(a.links).toContain(b._id);
     expect(b.links).not.toContain(a._id);
   });
 
-  test('/link single source:A target:B mode:non-recursive mono:false', () => {
+  test('/link single source:A target:B mode:non-recursive mono:false', async () => {
+    const repo = createChannelCacheRepo();
     const a = createChannel('A');
+    await repo.set(a._id, a);
     const b = createChannel('B');
-    const manager = new LinkManager([a, b]);
+    await repo.set(b._id, b);
+    const manager = new LinkManager(repo);
     const options: LinkOptions = {
       single: true,
       mode: 'non-recursive',
     };
 
-    manager.link([a, b], options);
+    await manager.link([a, b], options);
 
     // Expect A <-> B
     expect(a.links).toContain(b._id);
     expect(b.links).toContain(a._id);
   });
 
-  test('/link single source:A target:B mode:adjacent mono:true', () => {
+  test('/link single source:A target:B mode:adjacent mono:true', async () => {
     // Initial setup: A, B -> C -> D
+    const repo = createChannelCacheRepo();
     const a = createChannel('A');
+    await repo.set(a._id, a);
     const d = createChannel('D');
+    await repo.set(d._id, d);
     const c = createChannel('C', [d._id]); // C -> D
+    await repo.set(c._id, c);
     const b = createChannel('B', [c._id]); // B -> C
-    const manager = new LinkManager([a, b, c, d]);
+    await repo.set(b._id, b);
+    const manager = new LinkManager(repo);
     const options: LinkOptions = {
       single: true,
       mono: true,
       mode: 'adjacent',
     };
 
-    manager.link([a, b], options);
+    await manager.link([a, b], options);
 
     // Expect that A connects to B and C but NOT D and vice-versa
     expect(a.links).toContain(b._id);
@@ -68,19 +91,24 @@ describe('LinkManager', () => {
     expect(c.links).not.toContain(a._id);
   });
 
-  test('/link single source:A target:B mode:adjacent mono:false', () => {
+  test('/link single source:A target:B mode:adjacent mono:false', async () => {
     // Initial setup: A, B -> C -> D
+    const repo = createChannelCacheRepo();
     const a = createChannel('A');
+    await repo.set(a._id, a);
     const d = createChannel('D');
+    await repo.set(d._id, d);
     const c = createChannel('C', [d._id]); // C -> D
+    await repo.set(c._id, c);
     const b = createChannel('B', [c._id]); // B -> C
-    const manager = new LinkManager([a, b, c, d]);
+    await repo.set(b._id, b);
+    const manager = new LinkManager(repo);
     const options: LinkOptions = {
       single: true,
       mode: 'adjacent',
     };
 
-    manager.link([a, b], options);
+    await manager.link([a, b], options);
 
     // Expect that A connects to B and C but NOT D and vice-versa
     expect(a.links).toContain(b._id);
@@ -93,20 +121,25 @@ describe('LinkManager', () => {
     expect(c.links).toContain(a._id);
   });
 
-  test('/link single source:A target:B mode:recursive mono:true', () => {
+  test('/link single source:A target:B mode:recursive mono:true', async () => {
     // Initial setup: A, B -> C -> D
+    const repo = createChannelCacheRepo();
     const a = createChannel('A');
+    await repo.set(a._id, a);
     const d = createChannel('D');
+    await repo.set(d._id, d);
     const c = createChannel('C', [d._id]); // C -> D
+    await repo.set(c._id, c);
     const b = createChannel('B', [c._id]); // B -> C
-    const manager = new LinkManager([a, b, c, d]);
+    await repo.set(b._id, b);
+    const manager = new LinkManager(repo);
     const options: LinkOptions = {
       single: true,
       mono: true,
       mode: 'recursive',
     };
 
-    manager.link([a, b], options);
+    await manager.link([a, b], options);
 
     // Expect that A connects to B, C, and D
     expect(a.links).toContain(b._id);
@@ -119,19 +152,24 @@ describe('LinkManager', () => {
     expect(d.links).not.toContain(a._id);
   });
 
-  test('/link single source:A target:B mode:recursive mono:false', () => {
+  test('/link single source:A target:B mode:recursive mono:false', async () => {
     // Initial setup: A, B -> C -> D
+    const repo = createChannelCacheRepo();
     const a = createChannel('A');
+    await repo.set(a._id, a);
     const d = createChannel('D');
+    await repo.set(d._id, d);
     const c = createChannel('C', [d._id]); // C -> D
+    await repo.set(c._id, c);
     const b = createChannel('B', [c._id]); // B -> C
-    const manager = new LinkManager([a, b, c, d]);
+    await repo.set(b._id, b);
+    const manager = new LinkManager(repo);
     const options: LinkOptions = {
       single: true,
       mode: 'recursive',
     };
 
-    manager.link([a, b], options);
+    await manager.link([a, b], options);
 
     // Expect that A connects to B, C, and D
     expect(a.links).toContain(b._id);
@@ -144,15 +182,20 @@ describe('LinkManager', () => {
     expect(d.links).toContain(a._id);
   });
 
-  test('/link multiple A B C mode:non-recursive', () => {
+  test('/link multiple A B C mode:non-recursive', async () => {
     // Initial setup: A, B, C -> D
+    const repo = createChannelCacheRepo();
     const a = createChannel('A');
+    await repo.set(a._id, a);
     const b = createChannel('B');
+    await repo.set(b._id, b);
     const d = createChannel('D');
+    await repo.set(d._id, d);
     const c = createChannel('C', [d._id]); // C -> D
-    const manager = new LinkManager([a, b, c, d]);
+    await repo.set(c._id, c);
+    const manager = new LinkManager(repo);
 
-    manager.link([a, b, c], { mode: 'non-recursive' });
+    await manager.link([a, b, c], { mode: 'non-recursive' });
 
     // Expect that A connects to B and C but NOT D
     expect(a.links).toContain(b._id);
@@ -175,15 +218,20 @@ describe('LinkManager', () => {
     expect(d.links).not.toContain(c._id);
   });
 
-  test('/link multiple A B C mode:recursive', () => {
+  test('/link multiple A B C mode:recursive', async () => {
     // Initial setup: A, B, C -> D
+    const repo = createChannelCacheRepo();
     const a = createChannel('A');
+    await repo.set(a._id, a);
     const b = createChannel('B');
+    await repo.set(b._id, b);
     const d = createChannel('D');
+    await repo.set(d._id, d);
     const c = createChannel('C', [d._id]); // C -> D
-    const manager = new LinkManager([a, b, c, d]);
+    await repo.set(c._id, c);
+    const manager = new LinkManager(repo);
 
-    manager.link([a, b, c], { mode: 'recursive' });
+    await manager.link([a, b, c], { mode: 'recursive' });
 
     // Expect that A connects to B, C, and D
     expect(a.links).toContain(b._id);
@@ -204,5 +252,31 @@ describe('LinkManager', () => {
     expect(d.links).toContain(a._id);
     expect(d.links).toContain(b._id);
     expect(d.links).toContain(c._id);
+  });
+
+  test('that it links but DO NOT save to db', async () => {
+    // Since saving is something that should be done in bulk.
+    const repo = createChannelCacheRepo();
+    const a = createChannel('A');
+    await repo.set(a._id, a);
+    const b = createChannel('B');
+    await repo.set(b._id, b);
+    const manager = new LinkManager(repo);
+    const options: LinkOptions = {
+      single: true,
+      mode: 'non-recursive',
+    };
+
+    const localA = createChannel('A');
+    const localB = createChannel('B');
+    await manager.link([localA, localB], options);
+
+    // Expect A <-> B but NOT in db
+    expect(localA.links).toContain(b._id);
+    expect(localB.links).toContain(a._id);
+    const dbA = await repo.get(a._id, true);
+    expect(dbA.links).toHaveLength(0);
+    const dbB = await repo.get(b._id, true);
+    expect(dbB.links).toHaveLength(0);
   });
 });
--- packages/core/src/link/LinkManager.ts
+++ packages/core/src/link/LinkManager.ts
@@ -1,54 +1,38 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Channel } from '@zekuru-v2/entities';
-import { Snowflake } from '@zekuru-v2/types';
 import { LinkMode, LinkOptions } from './LinkOptions';
+import { ChannelCacheRepository } from '@zekuru-v2/repositories';
 
 export class LinkManager {
-  private map = new Map<Channel, Set<Channel>>();
+  constructor(private channelRepo: ChannelCacheRepository) {}
 
-  constructor(channels: Channel[]) {
-    const idToChannel = new Map<Snowflake, Channel>();
-
-    // Build lookup map
-    for (const ch of channels) {
-      idToChannel.set(ch._id, ch);
-    }
-
-    // Resolve links
-    for (const ch of channels) {
-      const linked = new Set<Channel>();
-
-      for (const id of ch.links) {
-        // TODO: What to do if channel is undefined?
-        const channel = idToChannel.get(id);
-        if (channel) linked.add(channel);
-      }
-
-      this.map.set(ch, linked);
-    }
-  }
-
-  link(channels: Channel[], options: LinkOptions): void {
+  async link(channels: Channel[], options: LinkOptions): Promise<void> {
     const sourceSet = options.single ? [channels[0]] : channels;
-    const targetSet = this._buildTargets(channels, options);
+    const targetSet = await this._buildTargets(channels, options);
 
+    const promises: ReturnType<typeof this._link>[] = [];
     for (const src of sourceSet) {
       for (const tgt of targetSet) {
-        if (src !== tgt) this._link(src, tgt, options.mono);
+        if (src !== tgt) promises.push(this._link(src, tgt, options.mono));
       }
     }
+    await Promise.all(promises);
   }
 
-  _buildTargets(channels: Channel[], options: LinkOptions): Set<Channel> {
+  private async _buildTargets(
+    channels: Channel[],
+    options: LinkOptions,
+  ): Promise<Set<Channel>> {
     let targetSet: Set<Channel>;
 
     if (options.single) {
-      targetSet = this._traverse(channels[1], options.mode);
+      targetSet = await this._traverse(channels[1], options.mode);
     } else {
       if (options.mode === 'recursive') {
         targetSet = new Set();
         for (const ch of channels) {
-          for (const r of this._traverse(ch, 'recursive')) targetSet.add(r);
+          for (const r of await this._traverse(ch, 'recursive'))
+            targetSet.add(r);
         }
       } else {
         targetSet = new Set(channels);
@@ -58,7 +42,10 @@ export class LinkManager {
     return targetSet;
   }
 
-  _traverse(root: Channel, mode: LinkMode): Set<Channel> {
+  private async _traverse(
+    root: Channel,
+    mode: LinkMode,
+  ): Promise<Set<Channel>> {
     const visited = new Set<Channel>();
     const stack = [root];
 
@@ -68,7 +55,10 @@ export class LinkManager {
 
       visited.add(current);
 
-      const neighbors = this.map.get(current) ?? new Set();
+      const channel = await this.channelRepo.get(current._id, true);
+      const neighbors = await Promise.all(
+        channel.links.map((link) => this.channelRepo.get(link, true)),
+      );
 
       if (mode === 'recursive') {
         for (const n of neighbors) if (!visited.has(n)) stack.push(n);
@@ -80,20 +70,21 @@ export class LinkManager {
     return visited;
   }
 
-  _link(source: Channel, target: Channel, mono = false): void {
+  private async _link(
+    source: Channel,
+    target: Channel,
+    mono = false,
+  ): Promise<void> {
     if (source._id === target._id) return; // Ignore same channel link
 
-    if (!this.map.has(source)) this.map.set(source, new Set());
-
-    const linked = this.map.get(source)!;
-    linked.add(target);
-    source.links = Array.from(linked).map((c) => c._id);
+    const sourceSet = new Set(source.links);
+    sourceSet.add(target._id);
+    source.links = Array.from(sourceSet);
 
     if (!mono) {
-      if (!this.map.has(target)) this.map.set(target, new Set());
-      const linked = this.map.get(target)!;
-      linked.add(source);
-      target.links = Array.from(linked).map((c) => c._id);
+      const targetSet = new Set(target.links);
+      targetSet.add(source._id);
+      target.links = Array.from(targetSet);
     }
   }
 }
